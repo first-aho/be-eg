f16bf46424697e3f4729e001a3b3c983d29a6c01
first-aho <140928368+first-aho@users.noreply.github.com>
2023-11-20 16:15:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
 from flask import Flask, jsonify
+
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 def config_app(app: Flask, base_url: str):
     app.config["BASE_URL"] = base_url
@@ -6,3 +11,10 @@ def config_app(app: Flask, base_url: str):
     @app.route("/")
     def index():
         return jsonify({"greet": "Hello from Colab!"})
+
+    @app.route("/books")
+    def get_books():
+        engine = create_engine(os.environ["DATABASE_CONNECTION_STRING"])
+        with engine.connect() as conn:
+            books = conn.execute(text("select * from book")).all()
+            return jsonify(books)
